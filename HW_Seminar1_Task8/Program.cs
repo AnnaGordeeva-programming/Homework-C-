e2a8106f5a2c6ff23241c04b03f8b969ba6b8206
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-31 16:43:51 +0600

    Альтернативное решение Task8 в комменте

--- HW_Seminar1_Task8/Program.cs
+++ HW_Seminar1_Task8/Program.cs
@@ -7,3 +7,11 @@ for(int i=1; i<=number; i++)
 {
     if(i%2 == 0) Console.Write($"{i} ");
 }
+
+//Можно сделать счетчик через 2 и начать с 2 сразу
+//Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//for(int i=2; i<=number; i=i+2)
+//{
+//    Console.Write($"{i} ");
+//}
