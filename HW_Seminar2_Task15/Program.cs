5cf2873ecab612ac0909345d9226038070d7e767
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-31 23:23:55 +0600

    HW_Seminar2_Task15

---
+++ HW_Seminar2_Task15/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+string day_number = " ";
+while(true)
+{
+    Console.Write("Введите номер дня недели (от 1 до 7), чтобы проверить, является ли он выходным: ");
+    day_number = Console.ReadLine();
+    switch(day_number)
+    {
+        case "1":
+        {
+            Console.WriteLine("Нет");
+            continue;
+        }
+        case "2":
+        {
+            Console.WriteLine("Нет");
+            continue;
+        }
+        case "3":
+        {
+            Console.WriteLine("Нет");
+            continue;
+        }
+        case "4":
+        {
+            Console.WriteLine("Нет");
+            continue;
+        }
+        case "5":
+        {
+            Console.WriteLine("Нет");
+            continue;
+        }
+        case "6":
+        {
+            Console.WriteLine("Да");
+            continue;
+        }
+        case "7":
+        {
+            Console.WriteLine("Да");
+            continue;
+        }
+        default:
+        {
+            Console.WriteLine("Неправильно задан номер");
+            break;
+        }
+    }
+}
