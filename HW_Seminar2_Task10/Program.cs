ddf9ff84ec82f401441a3e9fdc7a8aa14ec6187c
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-31 23:08:14 +0600

    HW_Seminar2_Tasks 10,13

---
+++ HW_Seminar2_Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<1000 && number>=100)
+{
+int a1 = number/100;
+int a2 = (number-a1*100)/10;
+Console.WriteLine(a2);
+}
+else Console.WriteLine("Некорректный ввод данных. Введите ТРЕХЗНАЧНОЕ число");
---
+++ HW_Seminar2_Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>=100)
+{
+int a3 = number%10;
+Console.WriteLine(a3);
+}
+else Console.WriteLine("Третьей цифры нет");
