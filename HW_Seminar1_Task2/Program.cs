16fb013edc51d2fcd15df2f440d0a5d8d4af68d1
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-31 13:15:43 +0600

    Tasks 2,4

---
+++ HW_Seminar1_Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2) Console.WriteLine($"max = {number1}");
+else Console.WriteLine($"max = {number2}");
---
+++ HW_Seminar1_Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int Max(int number1, int number2, int number3)
+{
+    int max = number1;
+    if(number2>max) max = number2;
+    if(number3>max) max = number3;
+    return max;
+}
+int max_number = Max(number1, number2, number3);
+Console.WriteLine($"max = {max_number}");
