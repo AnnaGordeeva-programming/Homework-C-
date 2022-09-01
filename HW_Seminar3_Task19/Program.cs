b2eab9d4bb678c774411d3332049b5edb4d581b2
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-01 12:46:23 +0600

    HW_Seminar3_Task 19

---
+++ HW_Seminar3_Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int number_length = number.Length;
+
+for(int i=0; i<number_length/2; i++)
+{
+    if(number[i] != number[number_length-i-1])
+    {
+        Console.WriteLine("Нет");
+    }
+    else Console.WriteLine("Да");
+}
