b7d3a5f1951b80dd6dad1a05117ca615bcebaf55
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-05 11:31:53 +0600

    Task 25

---
+++ HW_Seminar4_Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.Write("Введите число A: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+
+double Power(double numberA, double numberB)
+{
+    double power = Math.Pow(numberA, numberB);
+    return power;
+}
+
+Console.WriteLine(Power(numberA, numberB));
