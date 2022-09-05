79abf3bd3c1d9e64a7c3c9a66b913f27a21f2350
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-05 13:16:17 +0600

    TAsk 13 renewed

--- HW_Seminar2_Task13/Program.cs
+++ HW_Seminar2_Task13/Program.cs
@@ -1,11 +1,28 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа 
 //или сообщает, что третьей цифры нет.
-
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number>=100)
+
+int ThreeDigits(int number)
 {
-int a3 = number%10;
-Console.WriteLine(a3);
+int result = 0;
+while(number>=1000)
+{
+  result = number/10;
+  number = number/10;
+}
+return result;
+}
+
+int interim = ThreeDigits(number);
+if(number>=1000)
+{
+    int thirdNumber = interim % 10;
+    Console.WriteLine(thirdNumber);
+}
+else if(number>=100)
+{
+    int thirdNumber = number % 10;
+    Console.WriteLine(thirdNumber);
 }
 else Console.WriteLine("Третьей цифры нет");
