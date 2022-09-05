72dbb0722649e75714df986cac7bf315ba01272c
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-05 12:13:44 +0600

    HW_Seminar4

---
+++ HW_Seminar4_Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numA)
+{
+   int sum = 0;
+   while (numA>0)
+   {
+    sum = sum + numA%10;
+    numA = numA/10;
+   }
+    return sum;
+}
+
+Console.WriteLine(SumNumber(number));
---
+++ HW_Seminar4_Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+
+void FillArray(int[] array)
+{
+    int index = 0;
+    while(index<array.Length)
+    {
+        array[index] = new Random().Next(1,100);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int index = 0;
+    while(index<array.Length)
+    {
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+}
+
+int[] newArray = new int[10];
+FillArray(newArray);
+PrintArray(newArray);
