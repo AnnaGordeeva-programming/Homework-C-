bfe0ad50e527627941bae86a6d39aae41c348cf5
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-08 09:41:05 +0600

    HW_Seminar5

---
+++ HW_Seminar5_Task36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+    res[i] = new Random().Next(0, 100);
+    }
+    return res;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int pos = 0; pos < count; pos++)
+    {
+        Console.Write(col[pos] + " ");
+    }
+}
+
+int OddSum (int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i = i + 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[]array = GetArray(10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(OddSum(array));
---
+++ HW_Seminar5_Task38/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+    res[i] = new Random().Next(0, 100);
+    }
+    return res;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int pos = 0; pos < count; pos++)
+    {
+        Console.Write(col[pos] + " ");
+    }
+}
